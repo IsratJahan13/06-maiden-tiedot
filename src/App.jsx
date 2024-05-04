963d381451c787dbdae835fab4b08f2591f914d4
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-05-04 14:07:09 +0300

    step-1 2.18 has completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 const App = () => {
   const [countries,setCountries] = useState([])
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch('https://studies.cs.helsinki.fi/restcountries/api/all')
@@ -11,23 +12,43 @@ const App = () => {
     .then(data => setCountries(data))
   },[])
   console.log(countries)
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   
   return (
     <div>
       <h1>Country Information App</h1>
       <p>Search for countries:</p>
-      <div>
-        {countries.map(country => (
-          <Country key={country.name.common} 
-          name={country.name.common} 
-          capital={country.capital}
-          area={country.area} 
-          languages={country.languages ? Object.values(country.languages) : []}
-          flag={country.flags.svg}
-          />
-        ))}
-      </div>
-      
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleSearch}
+      />
+      {searchQuery && (
+        <div>
+        {filteredCountries.length > 10 ? (
+            <p>Too many matches, specify another filter</p>
+          ) : (
+            
+              filteredCountries.map(country => (
+              <Country key={country.name.common} 
+              name={country.name.common} 
+              capital={country.capital}
+              area={country.area} 
+              languages={country.languages ? Object.values(country.languages) : []}
+              flag={country.flags.svg}
+              />
+            ))
+          )}
+        </div>
+      )}
     </div>
   )
 }
